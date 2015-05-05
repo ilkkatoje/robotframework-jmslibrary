4a9eb513eee5d830be4a90fded364cbf148fcee6
ilkkatoje <ilkka.toje@gmail.com>
2015-05-05 23:24:28 +0300

    1.0.0

--- src/scripts/generate_documentation.sh
+++ src/scripts/generate_documentation.sh
@@ -5,11 +5,11 @@
 
 AMQ_JAR=$HOME/.m2/repository/org/apache/activemq/activemq-all/5.11.1/activemq-all-5.11.1.jar
 RF_JAR=$HOME/.m2/repository/org/robotframework/robotframework/2.8.7/robotframework-2.8.7.jar
-JMSLIB_VERSION=1.0.0-SNAPSHOT
+JMSLIB_VERSION=1.0.0
 
 SRC=../../src/main/java
 TARGET=../../target
 JMSLIB_JAR=$TARGET/robotframework-jmslibrary-$JMSLIB_VERSION.jar
 TOOLS_JAR=$JAVA_HOME/lib/tools.jar
 
-java -cp $TOOLS_JAR:$RF_JAR:$JMSLIB_JAR:$AMQ_JAR org.robotframework.RobotFramework libdoc $SRC/JMSLibrary.java $TARGET/robotframework-jmslibrary-$JMSLIB_VERSION.html
+java -cp $TOOLS_JAR:$RF_JAR:$JMSLIB_JAR:$AMQ_JAR org.robotframework.RobotFramework libdoc $SRC/JMSLibrary.java $TARGET/robotframework-jmslibrary.html
