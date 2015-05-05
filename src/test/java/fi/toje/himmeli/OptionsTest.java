69f6d578b35568a01af7da44a794c530c7dfbf8a
ilkkatoje <ilkka.toje@gmail.com>
2015-05-05 20:46:54 +0300

    renamed

--- src/test/java/fi/toje/himmeli/OptionsTest.java
+++ src/test/java/fi/toje/himmeli/OptionsTest.java
@@ -6,95 +6,94 @@ import javax.jms.Session;
 import org.junit.Assert;
 import org.junit.Test;
 
-import fi.toje.himmeli.jmslibrary.ProviderSession;
+import fi.toje.himmeli.jmslibrary.Options;
 
-public class ProviderSessionTest {
-/*
+public class OptionsTest {
+
 	@Test
 	public void convertDeliveryModePersistent() throws Exception {
-		int dm = ProviderSession.convertDeliveryMode("PERSISTENT");
+		int dm = Options.convertDeliveryMode("PERSISTENT");
 		Assert.assertEquals(dm, DeliveryMode.PERSISTENT);
 	}
 	
 	@Test
 	public void convertDeliveryModeNonPersistent() throws Exception {
-		int dm = ProviderSession.convertDeliveryMode("NON_PERSISTENT");
+		int dm = Options.convertDeliveryMode("NON_PERSISTENT");
 		Assert.assertEquals(dm, DeliveryMode.NON_PERSISTENT);
 	}
 	
 	@Test(expected=Exception.class)
 	public void convertDeliveryModeWrong() throws Exception {
-		ProviderSession.convertDeliveryMode("WRONG");
+		Options.convertDeliveryMode("WRONG");
 	}
 	
 	@Test
 	public void convertDeliveryModePersistentInt() throws Exception {
-		String dm = ProviderSession.convertDeliveryMode(DeliveryMode.PERSISTENT);
+		String dm = Options.convertDeliveryMode(DeliveryMode.PERSISTENT);
 		Assert.assertEquals(dm, "PERSISTENT");
 	}
 	
 	@Test
 	public void convertDeliveryModeNonPersistentInt() throws Exception {
-		String dm = ProviderSession.convertDeliveryMode(DeliveryMode.NON_PERSISTENT);
+		String dm = Options.convertDeliveryMode(DeliveryMode.NON_PERSISTENT);
 		Assert.assertEquals(dm, "NON_PERSISTENT");
 	}
 	
 	@Test(expected=Exception.class)
 	public void convertDeliveryModeWrongInt() throws Exception {
-		ProviderSession.convertDeliveryMode(123);
+		Options.convertDeliveryMode(123);
 	}
 	
 	@Test
 	public void convertType0() throws Exception {
-		int t = ProviderSession.convertType("0");
+		int t = Options.convertType("0");
 		Assert.assertEquals(t, Session.SESSION_TRANSACTED);
 	}
 	
 	@Test
 	public void convertType1() throws Exception {
-		int t = ProviderSession.convertType("1");
+		int t = Options.convertType("1");
 		Assert.assertEquals(t, Session.AUTO_ACKNOWLEDGE);
 	}
 	
 	@Test
 	public void convertType2() throws Exception {
-		int t = ProviderSession.convertType("2");
+		int t = Options.convertType("2");
 		Assert.assertEquals(t, Session.CLIENT_ACKNOWLEDGE);
 	}
 	
 	@Test
 	public void convertType3() throws Exception {
-		int t = ProviderSession.convertType("3");
+		int t = Options.convertType("3");
 		Assert.assertEquals(t, Session.DUPS_OK_ACKNOWLEDGE);
 	}
 	
 	@Test(expected=Exception.class)
 	public void convertType4() throws Exception {
-		ProviderSession.convertType("4");
+		Options.convertType("4");
 	}
 	
 	@Test
 	public void convertTypeSessionTransacted() throws Exception {
-		int t = ProviderSession.convertType(ProviderSession.SESSION_TRANSACTED);
+		int t = Options.convertType(Options.SESSION_TRANSACTED);
 		Assert.assertEquals(t, Session.SESSION_TRANSACTED);
 	}
 	
 	@Test
 	public void convertTypeAutoAcnowledge() throws Exception {
-		int t = ProviderSession.convertType(ProviderSession.AUTO_ACKNOWLEDGE);
+		int t = Options.convertType(Options.AUTO_ACKNOWLEDGE);
 		Assert.assertEquals(t, Session.AUTO_ACKNOWLEDGE);
 	}
 	
 	@Test
 	public void convertTypeClientAcknowledge() throws Exception {
-		int t = ProviderSession.convertType(ProviderSession.CLIENT_ACKNOWLEDGE);
+		int t = Options.convertType(Options.CLIENT_ACKNOWLEDGE);
 		Assert.assertEquals(t, Session.CLIENT_ACKNOWLEDGE);
 	}
 	
 	@Test
 	public void convertTypeDupsOkAcknowledge() throws Exception {
-		int t = ProviderSession.convertType(ProviderSession.DUPS_OK_ACKNOWLEDGE);
+		int t = Options.convertType(Options.DUPS_OK_ACKNOWLEDGE);
 		Assert.assertEquals(t, Session.DUPS_OK_ACKNOWLEDGE);
 	}
-	*/
 }
